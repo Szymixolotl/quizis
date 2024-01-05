56851d026c3e964e4925b9e6e37f1af075e1ed57
SutterSubmiss <84139532+SutterSubmiss@users.noreply.github.com>
2024-01-05 14:01:15 +0100

    Add files via upload

--- scriptmob.js
+++ scriptmob.js
@@ -1,5 +1,5 @@
-let myID = window.localStorage.getItem('myid');
-if(!myID){
-    myid = Math.floor(Math.random()*1000000000)+"_"+(new Date()).getTime;
-    window.localStorage.setItem('myid', myID);
+let myId = window.localStorage.getItem('myId');
+if(!myId){
+    myID = Math.floor(Math.random()*10000000)+"_"+(new Date()).getTime();
+    window.localStorage.setItem('myid',myId)
 }
